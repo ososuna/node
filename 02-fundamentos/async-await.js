0faf6a3d83633cf90b88ca26f3a796f149f2f87d
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-05 16:54:55 -0600

    Create async-await.js

---
+++ 02-fundamentos/async-await.js
@@ -0,0 +1,62 @@
+const empleados = [
+    {
+        id: 1,
+        nombre: 'Fernando'
+    },
+    {
+        id: 2,
+        nombre: 'Linda'
+    },
+    {
+        id: 3,
+        nombre: 'Karen'
+    }
+];
+
+const salarios = [
+    {
+        id: 1,
+        salario: 1000
+    },
+    {
+        id: 2,
+        salario: 1500
+    }
+];
+
+const getEmpleado = ( id ) => {
+    return new Promise( ( resolve, reject ) => {
+        const empleado = empleados.find( (e) => e.id === id )?.nombre;
+        ( empleado )
+            ? resolve( empleado )
+            : reject(`No existe empleado con id ${ id }`);
+    });
+}
+
+const getSalario = ( id ) => {
+    return new Promise( ( resolve, reject ) => {
+        const salario = salarios.find( (s) => s.id === id )?.salario;
+        ( salario )
+            ? resolve( salario )
+            : reject(`No existe salario para el id ${ id }`);
+    })
+}
+
+// async transforma una función para que regrese una promesa
+const getInfoUsuario = async( id ) => {
+    try {
+        const empleado = await getEmpleado( id );
+        const salario = await getSalario( id );
+        return `El salario del empleado ${ empleado } es de ${ salario }`;        
+    } catch (error) {
+        return error;        
+    }
+
+}
+
+const id = 4;
+
+getInfoUsuario( id )
+    .then( msg => console.log( msg ) )
+    .catch( err => console.log( err ) );
+
