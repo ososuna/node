96aeb3164f2a0016c22b6dc0e3a88695324917e5
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-06 13:01:18 -0600

    Bases de node

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,7 +1,12 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 
 console.clear();
-const base = 1;
+
+const [, , arg3 = 'base=5'] = process.argv;
+const [, base=5] = arg3.split('='); 
+console.log( base );
+
+// const base = 1;
 
 crearArchivo( base )
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
