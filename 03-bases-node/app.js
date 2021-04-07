b7a7bb2eb3244f72a7dbf4c65a65e70453f5a18e
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-07 13:07:07 -0600

    Bases de node

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,27 +1,10 @@
-const { options } = require('yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs')
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true        
-    })
-    .check( (argv) => {
-        if ( isNaN( argv.b ) ) {
-            throw 'La base tiene que ser un número';
-        }
-        return true;
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        default: false
-    })
-    .argv;
+const argv = require('./config/yargs');
+const colors = require('colors/safe'); 
 
 console.clear();
 
-crearArchivo( argv.b, argv.l )
-    .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+crearArchivo( argv.b, argv.l, argv.h )
+    .then( nombreArchivo => console.log(colors.green(nombreArchivo), 'creado') )
     .catch( err => console.log( err ) );
 
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,21 +1,24 @@
 const fs = require('fs');
+const colors = require('colors/safe');
 
-const crearArchivo = async ( base, listar ) => {
+const crearArchivo = async ( base, listar, hasta ) => {
     try {
         let salida = '';
-        
-        for (let i = 1; i <= 10; i++) {
+        let consola = '';
+
+        for (let i = 1; i <= hasta; i++) {
             salida += `${ base } * ${ i } = ${ base*i }\n`;
+            consola += `${ colors.red(base) } * ${ colors.blue(i) } = ${ colors.yellow(base*i) }\n`;
         }
 
         if ( listar ) {
             console.log('=================');
-            console.log('Tabla del', base);
+            console.log('Tabla del', colors.red(base));
             console.log('=================');
-            console.log( salida );
+            console.log( consola );
         }
         const nombreArchivo = `tabla-${ base }.txt`;
-        fs.writeFileSync( nombreArchivo, salida );
+        fs.writeFileSync( `./salida/${ nombreArchivo }`, salida );
         return nombreArchivo;
     } catch ( err ) {
         throw err;
