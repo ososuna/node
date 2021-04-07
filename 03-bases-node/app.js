f2c12d08698ad1cc9d593c45abe7967a1605a2fd
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-06 22:17:58 -0600

    Bases de node

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,14 +1,27 @@
+const { options } = require('yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true        
+    })
+    .check( (argv) => {
+        if ( isNaN( argv.b ) ) {
+            throw 'La base tiene que ser un número';
+        }
+        return true;
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        default: false
+    })
+    .argv;
 
 console.clear();
 
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base=5] = arg3.split('='); 
-console.log( base );
-
-// const base = 1;
-
-crearArchivo( base )
+crearArchivo( argv.b, argv.l )
     .then( nombreArchivo => console.log(nombreArchivo, 'creado') )
     .catch( err => console.log( err ) );
 
