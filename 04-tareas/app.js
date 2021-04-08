3d2ee7f3280a4a9f1faddc63a7f956946b9785b4
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-08 12:25:23 -0600

    Tareas Update 2

--- 04-tareas/app.js
+++ 04-tareas/app.js
@@ -1,8 +1,17 @@
 require('colors');
+const { inquirerMenu } = require('./helpers/inquirer');
+
 console.clear();
 
 const main = async () => {
-    console.log('Hola mundo');
+    
+    let opt = '';
+
+    do {
+        opt = await inquirerMenu();
+        console.log( opt );
+    } while ( opt !== '0' );
+    
 }
 
 main();
