653ea384e6a4f5341fd8e08bf2e4bdffd7f3968e
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-09 12:11:07 -0600

    Tareas Update 3

--- 04-tareas/app.js
+++ 04-tareas/app.js
@@ -1,15 +1,45 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
-
-console.clear();
+const { inquirerMenu, pause, leerInput } = require('./helpers/inquirer');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
 
 const main = async () => {
     
     let opt = '';
+    const tareas = new Tareas();
 
     do {
+        console.log('\n');
         opt = await inquirerMenu();
-        console.log( opt );
+
+        switch ( opt ) {
+            case '1':
+                const desc = await leerInput('Descripción: ');
+                tareas.crearTarea( desc );
+                break;
+            case '2':
+                console.log( tareas._listado );
+                break;
+            case '3':
+                
+                break;
+            case '4':
+            
+                break;
+            case '5':
+        
+                break;
+            case '6':
+    
+                break;
+            case '7':
+                
+                break;
+            default:
+                break;
+        }
+
+        if( opt !== '0' ) await pause();
     } while ( opt !== '0' );
     
 }
--- 04-tareas/helpers/inquirer.js
+++ 04-tareas/helpers/inquirer.js
@@ -6,7 +6,44 @@ const preguntas = [
         type: 'list',
         name: 'opcion',
         message: 'Selecciona una opción',
-        choices: ['opt1', 'opt2', 'opt3']
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tarea'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completadas'
+            },
+            {
+                value: '4',
+                name: '4. Listar tareas pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar tarea(s)'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tarea'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            },
+        ]
+    }
+];
+
+const preguntaPausa = [
+    {
+        type: 'input',
+        name: 'pausa',
+        message: `Presiona ${ 'enter'.rainbow } para continuar`
     }
 ];
 
@@ -16,11 +53,36 @@ const inquirerMenu = async() => {
     console.log('Seleccione una opción'.green);
     console.log('======================\n'.green);
 
-    const opt = await inquirer.prompt( preguntas );
+    const { opcion } = await inquirer.prompt( preguntas );
 
-    return opt;
+    return opcion;
+}
+
+const pause = async() => {
+    await inquirer.prompt( preguntaPausa );
+}
+
+const leerInput = async( message ) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate( value ) {
+                if ( value.length === 0 ) {
+                    return 'Por favor ingresa un valor';
+                }
+                return true;
+            }
+        }
+    ];
+
+    const { desc } = await inquirer.prompt( question );
+    return desc;
 }
 
 module.exports = {
-    inquirerMenu
+    inquirerMenu,
+    pause,
+    leerInput
 }
