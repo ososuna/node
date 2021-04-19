ccb753ad6475f00ccf0b312ad6af525b42b66ccf
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-04-19 12:15:29 -0500

    Clima app Update 1

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 class Busquedas {
 
     historial = [];
@@ -6,10 +8,36 @@ class Busquedas {
         // leer DB si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1Ijoib3Nvc3VuYSIsImEiOiJja25vdWE1aDAxYWwzMnBwZG84dmN6NW9oIn0.E45yOK4sb9Y-LOmjKmudyg',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad( lugar = '' ) {
-        // petición http
-        console.log( lugar );
-        return []; // retornar los lugares 
+
+        try {
+            
+            // petición http
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${ lugar }.json `,
+                params: this.paramsMapbox
+            });
+            
+            const resp = await instance.get();
+
+            console.log( resp.data );
+            
+            return []; // retornar los lugares 
+        
+        } catch ( err ) {
+            
+            return [];
+        
+        }
+
     }
 
 }
