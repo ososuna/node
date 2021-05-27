8c0c3ec48bab499eb13a017a42417f0126280fc5
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-05-27 17:01:28 -0500

    Google SignIn added

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async ( req, res = response ) => {
 
@@ -47,14 +48,53 @@ const login = async ( req, res = response ) => {
     }
 }
 
-const googleSignIn = ( req = request, res = response ) => {
+const googleSignIn = async ( req = request, res = response ) => {
 
     const { id_token } = req.body;
 
-    res.json({
-        msg: 'Ok Google SignIn',
-        id_token
-    });
+    try {
+
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if ( !usuario ) {
+            // Crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        
+        }
+
+        // Si el usuario en DB
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Usuario bloqueado'
+             });
+        }
+
+        // Generar JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+            
+    } catch ( error ) {
+        
+        res.status(400).json({
+            msg: 'Token de Google no válido'
+        });
+        
+    }
 
 }
 
