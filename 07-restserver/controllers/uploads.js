3d06f032091d52691668f2c6c2f8323a56810450
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2021-06-03 14:55:50 -0500

    Inicio subir archivos

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,25 +1,19 @@
 
-const path = require('path');
 const { request, response } = require("express");
+const { subirArchivo } = require("../helpers");
 
-const cargarArchivo = ( req = request, res = response ) => {
+const cargarArchivo = async ( req = request, res = response ) => {
 
     if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo ) {
         return res.status(400).json({ msg: 'No hay archivos en la petición' });
     }
 
-    const { archivo } = req.files;
+    // Imágenes
+    const nombre = await subirArchivo( req.files );
 
-    const uploadPath = path.join( __dirname, '../uploads/' + archivo.name );
-  
-    archivo.mv(uploadPath, ( err ) => {
-      
-        if ( err ) {
-            return res.status(500).json({ err });
-        }
-    
-        res.json({ msg: 'File uploaded to ' + uploadPath });
-    });
+    res.json({
+        nombre
+    })
 
 } 
 
